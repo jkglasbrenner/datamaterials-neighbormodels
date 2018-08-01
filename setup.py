271d36eae220c993196ddd1ce9dd00e76f14b72f
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-08-01 16:38:49 -0400

    Update required packages list and required python version   * Use Python 3.6 and above so variable typing is available.   * Specify numpy and pandas as package requirements   * Remove version requirement from pymatgen

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ setuptools.setup(
     author_email="jglasbr2@gmu.edu",
     license="MIT",
     version="0.0.1",
-    description=("Counts the number of pairwise neighbors in a lattice within "
-                 "and between sub-lattices."),
-    python_requires="~=2.7.0, >=3.5",
+    description=(
+        "Counts the number of pairwise neighbors in a lattice within "
+        "and between sub-lattices."
+    ),
+    python_requires=">=3.6",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
-        "pymatgen >= 2018.4.20",
+        "numpy",
+        "pandas",
+        "pymatgen",
     ],
 )
