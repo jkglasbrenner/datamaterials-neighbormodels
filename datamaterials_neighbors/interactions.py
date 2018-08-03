c3bc6caf24a915edf4479de4657c1ca17552ad6c
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-08-03 18:49:27 -0400

    Finished interaction module, which included some refactoring   * Takes the NeighborData and MagneticPatterns objects and automatically     creates the model matrix for a pairwise interaction model.   * Interaction parameter names automatically generated from subspecie labels.   * Refactored data frame manipulations to take advantage of piping     functionality.   * Divided the model coefficients pipeline into smaller chunks within several     functions.   * (Still needed) Documentation on a few of the piping functions.

--- datamaterials_neighbors/interactions.py
+++ datamaterials_neighbors/interactions.py
@@ -3,48 +3,133 @@
 from typing import Dict, Union
 
 import numpy as np
-from pandas import DataFrame
+import pandas as pd
+from pandas import DataFrame, Series
+
+from datamaterials_neighbors.neighbors import NeighborData
 
 MagneticPatterns = Dict[str, Union[int, float]]
 
 
-def get_interaction_coefficients(
-    neighbors_df: DataFrame,
-    pairwise_interactions_df: DataFrame,
-    bin_to_distance_map: DataFrame,
+def build_model(
+    neighbor_data: NeighborData,
+    magnetic_patterns: MagneticPatterns,
+) -> DataFrame:
+    """Builds and returns a data frame describing a pairwise interaction model. The
+    intended use-case for the model is fitting magnetic energies taken from density
+    functional theory calculations and extracting exchange parameters.
+
+    :param neighbor_data: A named tuple with three field names:
+
+        ``data_frame``
+            A pandas ``DataFrame`` of neighbor counts aggregated over site-index pairs
+            and separation distances.
+
+        ``bins_data_frame``
+            A pandas ``DataFrame`` of neighbor distances mapped to unique bin
+            intervals.
+
+        ``structure``
+            A copy of the ``Structure`` object defining the crystal structure.
+    :param magnetic_patterns: A dictionary of magnetic patterns to be mapped onto
+        the crystal structure and used to compute the interaction coefficients of the
+        model.
+    :return: A pandas ``DataFrame`` of the interaction parameter names and coefficients
+        for the pairwise interaction model.
+    """
+    magnetic_patterns_df: DataFrame = build_magnetic_patterns_data_frame(
+        magnetic_patterns=magnetic_patterns
+    )
+
+    model_coefficients_df: DataFrame = magnetic_patterns_df \
+        .pipe(compute_interaction_signs) \
+        .pipe(compute_model_coefficients, neighbor_data=neighbor_data) \
+        .pivot(index="pattern", columns="parameter_name", values="coefficient") \
+        .reset_index()
+
+    model_coefficients_df.columns.name = ""
+
+    return model_coefficients_df
+
+
+def compute_model_coefficients(
+    interaction_signs_df: DataFrame,
+    neighbor_data: NeighborData,
+) -> DataFrame:
+    """Computes the model coefficients by aggregating over the dot product of the
+    neighbor counts and interaction signs.
+
+    :param interaction_signs_df: A pandas ``DataFrame`` of the signs of the pairwise
+        interactions.
+    :param neighbor_data: A named tuple with three field names:
+
+        ``data_frame``
+            A pandas ``DataFrame`` of neighbor counts aggregated over site-index pairs
+            and separation distances.
+
+        ``bins_data_frame``
+            A pandas ``DataFrame`` of neighbor distances mapped to unique bin
+            intervals.
+
+        ``structure``
+            A copy of the ``Structure`` object defining the crystal structure.
+    :return: A pandas ``DataFrame`` of the model coefficients.
+    """
+    return neighbor_data.data_frame \
+        .pipe(multiply_interaction_signs_and_neighbor_count,
+              interaction_signs_df=interaction_signs_df) \
+        .pipe(group_subspecie_pairs_and_rank_by_distance) \
+        .pipe(label_interaction_parameters) \
+        .pipe(aggregate_interaction_coefficients,
+              num_sites=neighbor_data.structure.num_sites)
+
+
+def multiply_interaction_signs_and_neighbor_count(
+    data_frame: DataFrame,
+    interaction_signs_df: DataFrame,
+) -> DataFrame:
+    return data_frame \
+        .merge(interaction_signs_df, on=["i", "j"]) \
+        .sort_values("pattern") \
+        .assign(coefficient=lambda x: x["sign"] * x["n"])
+
+
+def group_subspecie_pairs_and_rank_by_distance(data_frame: DataFrame) -> DataFrame:
+    df: DataFrame = data_frame.copy()
+    df["rank"] = df \
+        .groupby(["subspecies_i", "subspecies_j"]) \
+        .apply(lambda x: x[["distance_bin"]].rank(method="dense")) \
+        .apply(lambda x: pd.to_numeric(arg=x, downcast="integer"))
+
+    return df
+
+
+def label_interaction_parameters(data_frame: DataFrame) -> DataFrame:
+    df = data_frame.copy()
+
+    parameter_names: Series = "J" + df["rank"].astype(str)
+
+    single_specie_check: Series = df["subspecies_i"] == df["subspecies_j"]
+    single_specie: bool = single_specie_check.all()
+
+    if not single_specie:
+        parameter_names += "_" + df["subspecies_i"] + df["subspecies_j"]
+
+    df["parameter_name"] = parameter_names
+
+    return df
+
+
+def aggregate_interaction_coefficients(
+    data_frame: DataFrame,
     num_sites: int,
 ) -> DataFrame:
-    """Computes interaction coefficients using the neighbors and interactions data
-    frames.
-
-    :param neighbors_df: A pandas ``DataFrame`` of neighbor counts aggregated over
-        site-index pairs and separation distances.
-    :param pairwise_interactions_df: A pandas ``DataFrame`` of pairwise interaction terms.
-    :param bin_to_distance_map:
-    :param num_sites:
-    :return: A pandas ``DataFrame`` of interaction coefficients.
-    """
-    coefficients_df: DataFrame = (
-        neighbors_df
-            .merge(pairwise_interactions_df, on=["i", "j"])
-            .sort_values("pattern")
-            .assign(coefficient=lambda x: x["interaction"] * x["n"])
-            .assign(rank=lambda x: x.groupby(["pattern", "subspecies_i",
-                                              "subspecies_j"])
-                                    .apply(lambda x: x.loc[:, ["distance_bin"]]
-                                                      .rank(method="dense")))
-            .assign(rank=lambda x: pd.to_numeric(x["rank"], downcast="integer"))
-            .groupby(["pattern", "distance_bin", "rank", "subspecies_i", "subspecies_j"])
-            .apply(lambda x: np.sum(x[["coefficient"]]) / num_sites)
-            .reset_index()
-            .merge(bin_to_distance_map, on=["distance_bin"])
-            .sort_values(by=["pattern", "subspecies_i", "subspecies_j",
-                             "distance_bin"])
-            .loc[:, ["pattern", "subspecies_i", "subspecies_j", "rank",
-                     "distance_ij", "coefficient"]]
-    )  # yapf: disable
-
-    return coefficients_df
+    return data_frame \
+        .groupby(["pattern", "parameter_name"]) \
+        .apply(lambda x: np.sum(x[["coefficient"]]) / num_sites) \
+        .reset_index() \
+        .sort_values(by=["pattern", "parameter_name"]) \
+        .loc[:, ["pattern", "parameter_name", "coefficient"]]
 
 
 def build_magnetic_patterns_data_frame(
@@ -53,39 +138,34 @@ def build_magnetic_patterns_data_frame(
     """Converts dictionary of magnetic patterns into the pandas ``DataFrame``
     format.
 
-    :param magnetic_configurations:
-    :return: A pandas ``DataFrame`` labeling and specifying magnetic configurations.
+    :param magnetic_patterns: A dictionary of magnetic patterns to be mapped onto
+        the crystal structure and used to compute the interaction coefficients of the
+        model.
+    :return: A pandas ``DataFrame`` labeling and specifying magnetic patterns.
     """
-    magnetic_patterns_df: DataFrame = (
-        DataFrame(data=magnetic_patterns)
-            .reset_index(level=0)
-            .rename(columns={"index": "site"})
-            .melt(id_vars=["site"],
-                  value_vars=list(magnetic_patterns.keys()),
-                  var_name="pattern",
-                  value_name="spin")
-            .loc[:, ["pattern", "site", "spin"]]
-    )  # yapf: disable
-
-    return magnetic_patterns_df
+    return DataFrame(data=magnetic_patterns) \
+        .reset_index(level=0) \
+        .rename(columns={"index": "site"}) \
+        .melt(id_vars=["site"],
+              value_vars=list(magnetic_patterns.keys()),
+              var_name="pattern",
+              value_name="spin") \
+        .loc[:, ["pattern", "site", "spin"]]
 
 
-def get_pairwise_interactions(magnetic_patterns_df: DataFrame, ) -> DataFrame:
-    """Builds data frame of pairwise interaction terms.
+def compute_interaction_signs(magnetic_patterns_df: DataFrame) -> DataFrame:
+    """Computes the signs of the pairwise interactions for the magnetic model.
 
     :param magnetic_patterns_df: A pandas ``DataFrame`` labeling and specifying
-        magnetic configurations.
-    :return: A pandas ``DataFrame`` of pairwise interaction terms.
+        magnetic patterns.
+    :return: A pandas ``DataFrame`` of the signs of the pairwise interactions.
     """
-    pairwise_interactions_df: DataFrame = (
-        magnetic_patterns_df
-            .merge(right=magnetic_patterns_df, how="inner", on="pattern",
-                   suffixes=("_i", "_j"))
-            .rename(columns={"site_i": "i", "site_j": "j"})
-            .loc[:, ["pattern", "i", "j", "spin_i", "spin_j"]]
-            .assign(interaction=lambda x: x["spin_i"] * x["spin_j"])
-            .query("i <= j")
-    )  # yapf: disable
-
-    return pairwise_interactions_df
-
+    return magnetic_patterns_df \
+        .merge(right=magnetic_patterns_df,
+               how="inner",
+               on="pattern",
+               suffixes=("_i", "_j")) \
+        .rename(columns={"site_i": "i", "site_j": "j"}) \
+        .loc[:, ["pattern", "i", "j", "spin_i", "spin_j"]] \
+        .assign(sign=lambda x: x["spin_i"] * x["spin_j"]) \
+        .query("i <= j")
