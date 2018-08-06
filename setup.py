bc512ad792aa7eaa9b36b97eaacff6879dcb3ace
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-08-06 17:41:45 -0400

    Update setup file and project metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
 
 import setuptools
 from sphinx.setup_command import BuildDoc
+
+with open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
 
 name = "neighbormodels"
 version = "0.0"
@@ -13,19 +19,29 @@ setuptools.setup(
     author_email="jglasbr2@gmu.edu",
     license="MIT",
     version=release,
+    url="https://github.com/jkglasbrenner/datamaterials-neighbormodels",
+    project_urls=OrderedDict((
+        ("Documentation", "https://neighbormodels.readthedocs.io"),
+        ("Code", "https://github.com/jkglasbrenner/datamaterials-neighbormodels"),
+    )),
     description=(
         "A tool for building crystal lattice models with distance-dependent "
         "neighbor interactions."
     ),
+    long_description=readme,
     python_requires=">=3.6",
-    packages=setuptools.find_packages("src"),
-    package_dir={"": "src"},
+    packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
         "numpy",
         "pandas",
         "pymatgen",
     ],
+    extras_require={
+        "docs": [
+            "sphinx",
+        ],
+    },
     cmdclass={"build_sphinx": BuildDoc},
     command_options={
         "build_sphinx": {
